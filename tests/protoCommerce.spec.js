bc26ace6851bcc0ed00b6f6ac2314c12414a08af
Aanchal <aanchalverma.av@outlook.com>
2025-03-04 18:32:00 +0530

    Added test for more validations

--- tests/protoCommerce.spec.js
+++ tests/protoCommerce.spec.js
@@ -2,11 +2,33 @@ const {test,expect} = require('@playwright/test')
 
 
 
-test.only('proto commerce app login', async({page})=>{
-page.goto('https://rahulshettyacademy.com/angularpractice/')
+test('using getByLabel operate on elements', async({page})=>{
+await page.goto('https://rahulshettyacademy.com/angularpractice/')
 
 //verify the labels using getByLabel
-page.getByLabel('Check me out if you Love IceCreams!').click()
-page.getByLabel('Employed').click()
+await page.getByLabel('Check me out if you Love IceCreams!').click()
+await page.getByLabel('Employed').click()
+await page.getByLabel('Gender').selectOption('Female')
+
+})
+
+test('using getByPlaceholder, getByText, getByRole perform chaining methods', async({page})=>{
+    await page.goto('https://rahulshettyacademy.com/angularpractice/')
+    
+    //verify using getByPlaceholder
+    await page.getByPlaceholder('Password').fill('Test@1234')
+
+    //verify using getByRole
+    await page.getByRole('button',{name: 'Submit'}).click()
+    
+    //verify using getByText
+    await page.getByText(" The Form has been submitted successfully!.").isVisible()
+})
+
+test('validate shop page and chain methods', async({page})=>{
+    await page.goto('https://rahulshettyacademy.com/angularpractice/')
+    await page.getByText('Shop').click()
+    await page.locator("app-card").filter({hasText: 'Nokia Edge'}).getByRole('button').click()
+
 
 })
