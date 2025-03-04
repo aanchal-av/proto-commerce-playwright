8c7b7eb2d75755a998b954aacf0c9593caf72959
Aanchal <aanchalverma.av@outlook.com>
2025-03-04 22:48:03 +0530

    added test for validating alerts

--- tests/practicePage.spec.js
+++ tests/practicePage.spec.js
@@ -1,10 +1,26 @@
 const {test,expect} = require('@playwright/test')
 
-test.only('navigate back and forward ', async({page})=>{
-
+test('navigate back and forward ', async({page})=>{
     await page.goto('https://rahulshettyacademy.com/AutomationPractice/')
     await page.goto('https://google.com')
     await page.goBack()
     await page.goForward()
 
 })
+
+test('validate the hidden and non hidden elements', async({page})=>{
+    await page.goto('https://rahulshettyacademy.com/AutomationPractice/')
+    await page.locator('#hide-textbox').click()
+    // await page.locator('input[style="display: none;"]')
+    await page.locator('#displayed-text').isHidden()
+    await page.locator('#show-textbox').click()
+    await page.locator('#displayed-text').isVisible()
+
+})
+
+test('validate the pop up', async({page})=>{
+    await page.goto('https://rahulshettyacademy.com/AutomationPractice/')
+    await page.locator('#alertbtn').click()
+    page.on('dialog',dialog=> dialog.accept)
+})
+
