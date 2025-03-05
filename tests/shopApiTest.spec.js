7b6744fec241f058c9a46920ea9d2a671e3e15ab
Aanchal <aanchalverma.av@outlook.com>
2025-03-05 18:47:25 +0530

    updated shopapi test

---
+++ tests/shopApiTest.spec.js
@@ -0,0 +1,34 @@
+const {test,expect,request} = require('@playwright/test')
+const loginPayload = {userEmail: 'aanchal.av@dummy.com', userPassword: 'Test@1234'}
+let token
+test.beforeAll(  async()=>{
+    
+   const apiContext= await request.newContext()
+   const loginResponse = await apiContext.post('https://rahulshettyacademy.com/api/ecom/auth/login',
+    {
+        data: loginPayload
+    }
+   )
+   expect(loginResponse.ok()).toBeTruthy()
+   const loginResponseJson = loginResponse.json()
+    token = loginResponseJson.token
+
+})
+
+test.beforeEach( ()=>{
+
+
+})
+
+
+test.only('Client app login', async({page})=>{
+    page.addInitScript(value =>{
+        window.localStorage.setItem('token',value)
+    }, token)
+    await page.goto('https://rahulshettyacademy.com/client')
+
+
+
+
+
+})
